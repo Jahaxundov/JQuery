c505d82a18f7c72c2fd8ba5d4af9735dd111ef12
Jahaxundov <jahangirea@code.edu.az>
2023-09-28 08:18:27 +0400

    added

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+$(function () {
+    $(".tab-header .item");
+    $(".tab-content .item");
+    let tab = document.querySelectorAll(".tab-content .item");
+
+
+    $(".tab-header .item").click(function () {
+        $(".active-tab").removeClass("active-tab");
+        $(this).addClass("active-tab");
+
+
+        tab.forEach(content => {
+            if (content.getAttribute("data-id") == this.getAttribute("data-id")) {
+                content.classList.remove("d-none")
+            }
+            else {
+                content.classList.add("d-none")
+            }
+        });
+    });
+})
+
+
+
+
+
+
+
+
